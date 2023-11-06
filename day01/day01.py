871fc010a6f83bda46f3f9fdabe4f3b6fea03bf1
Alex Nazareth <anazareth@ryerson.ca>
2023-11-06 15:52:12 -0500

    day 1 both parts complete

--- day01/day01.py
+++ day01/day01.py
@@ -1,7 +1,36 @@
+"""
+Advent of Code 2022 Day 01
+https://adventofcode.com/2022/day/1
+AUTHOR: Alex Nazareth
+DATE:   November 2023
+Short description:
+ - Input text list of integers, separated by \n.
+ - Part 1: Return highest sum of each group of integers separated by a blank line.
+ - Part 2: Return sum of top 3 highest.
+"""
+
 import pandas as pd
 
-in_url = "https://adventofcode.com/2022/day/1/input.txt"
+infile = "input.txt"
 
-data = pd.read_csv(in_url)
+data = pd.read_csv(infile, header=None, skip_blank_lines=False)
 
-print(data[:5])
+max_cal = [0, 0, 0]
+cal_count = 0
+for cal in data[0]:
+    if cal > 0:
+        cal_count = cal_count + cal
+    else:
+        if cal_count>max_cal[2]:
+            if cal_count>max_cal[1]:
+                if cal_count>max_cal[0]:
+                    max_cal.insert(0, cal_count)
+                else:
+                    max_cal.insert(1, cal_count)
+            else:
+                    max_cal.insert(2, cal_count)
+            max_cal.pop()
+        cal_count = 0
+
+print(max_cal)
+print(sum(max_cal))
