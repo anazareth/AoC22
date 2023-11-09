c1c67e2095b5d9407970652ed3c7361175ee21c2
Alex Nazareth <anazareth@ryerson.ca>
2023-11-09 18:02:46 -0500

    day 6 part 2 complete

--- day06/day06.py
+++ day06/day06.py
@@ -6,19 +6,18 @@ DATE:   November 2023
 Short description:
 - Find Start of Packet Marker (SPM), first group of 4 sequential different characters.
 - Part 1: Find position of first character of first SPM.
-- Part 2:
+- Part 2: 14 characters instead of 4.
 """
 
 import pandas as pd
 
-def part1(data):
-    spm_size = 4
-    four_char = data[:spm_size]
-    i = spm_size
-    while len(set(four_char)) != 4:
+def main(data, mm_size=4):
+    four_char = data[:mm_size]
+    i = mm_size
+    while len(set(four_char)) != mm_size:
         four_char = four_char[1:] + data[i]
         i += 1
-    if len(set(four_char)) == spm_size:
+    if len(set(four_char)) == mm_size:
         print(four_char)
         return i
     else:
@@ -26,11 +25,8 @@ def part1(data):
 
 if __name__ == '__main__':
     infile = 'input.txt'
-    # infile = 'day06\input.txt'
     indata = pd.read_csv(infile, header=None, dtype=str, skip_blank_lines=False)
     indata = indata[0][0]
-    print(indata[:4])
-    print(indata[4])
 
-    print("Part 1: The result is: " + str(part1(indata)))
-    # print("Part 2: The result is: " + str(part2(indata)))
+    print("Part 1: The result is: " + str(main(indata, 4)))
+    print("Part 2: The result is: " + str(main(indata, 14)))
