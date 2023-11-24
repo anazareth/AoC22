a6f7b9a38327bbd13720abb9b7cb3640c7ffdd1b
Alex Nazareth <anazareth@ryerson.ca>
2023-11-24 13:22:59 -0500

    update comments for day 7

--- day07/day07.py
+++ day07/day07.py
@@ -4,18 +4,20 @@ https://adventofcode.com/2022/day/7
 AUTHOR: Alex Nazareth
 DATE:   November 2023
 Short description:
-- Given input of filesystem navigation,
-    find sum of all directories with filesize =< 100,000.
+- Given input of filesystem navigation.
+- Part 1: Find sum of all directories with filesize < 100,000.
+- Part 2: Find smallest directory that when deleted will free up at least 30,000,000 units.
+-- ie Size of deleted directory + size of free space (out of 70,000,000 total) >= 30,000,000. 
 """
 
 from collections import defaultdict
 
 def main(data, isPart1 = True):
     dirs = defaultdict(int)
-    cwd = '/'
-    data.readline()
+    cwd = '/'  # starting with outermost directory
+    data.readline()  # omit first line containing '$ cd /'
     for r in data:
-        parts = r[:-1].split(' ')
+        parts = r[:-1].split(' ')  # omit newline character
         if parts[0] == '$':
             if parts[1] == 'cd':
                 if parts[2] == '..':
@@ -24,11 +26,12 @@ def main(data, isPart1 = True):
                     cwd = cwd + parts[2] + '/'
             elif parts[1] == 'ls':
                 pass
-        elif parts[0] == 'dir':
+        elif parts[0] == 'dir':  # row contains directory info, no action necessary
             pass
-        else:
+        else:  # row contains file info (size)
             filesize = parts[0]
             for i in range(1, cwd.count('/') + 1):
+                # add filesize to all ancestral directories
                 dirname = '/'.join(cwd.split('/')[:-i]) + '/'
                 dirs[dirname] += int(filesize)
     sum_small_dirs = sum([d for d in dirs.values() if d<100000])
@@ -39,15 +42,15 @@ def main(data, isPart1 = True):
         UPDATE_SPACE_REQ = 30000000
         free_space = TOTAL_FS_SIZE - dirs['/']
         min_filesize_req = UPDATE_SPACE_REQ - free_space
-        # print("Min filesize required: " + str(dirs[0]))
         return min([d for d in dirs.values() if d>=min_filesize_req])
 
 if __name__ == '__main__':
     infile = 'input.txt'
     indata = open(infile, 'r')
-    print("Part 1: The result is: " + str(main(indata)))
+    print(f"Part 1: The sum of all directories less than size 100,000 is: {str(main(indata))} units")
     indata.close()
     indata = open(infile, 'r')
-    print("Part 2: The result is: " + str(main(indata, False)))
+    print("Part 2: To yield 30,000,000 units of free space, " +
+          "the smallest directory that can be deleted is of size: " + str(main(indata, False)))
     indata.close()
     
